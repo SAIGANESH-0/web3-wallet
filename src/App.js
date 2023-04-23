c8f25be773d432c4e8f41e4db27f22804dd288af
saiganeshvoona <saiganeshvoona4@gmail.com>
2023-04-23 19:04:07 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Select } from "antd";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import Recover from "./components/Recover";
+import Wallet from "./components/Wallet";
 
 function App() {
+  const [wallet, setWallet] = useState(null);
+  const [seedPhrase, setSeedPhrase] = useState(null);
+  const [selectedChain, setSelectedChain] = useState("0x1");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="headerLogo"> Wallet</h1>
+        <Select
+          onChange={(val) => setSelectedChain(val)}
+          value={selectedChain}
+          options={[
+            {
+              label: "Ethereum Mainnet",
+              value: "0x1",
+            },
+            {
+              label: "Polygon Testnet",
+              value: "0x13881",
+            },
+          ]}
+          className="dropdown"
+        ></Select>
       </header>
+      {wallet && seedPhrase ? (
+        <Routes>
+          <Route
+            path="/yourwallet"
+            element={
+              <Wallet
+                wallet={wallet}
+                setWallet={setWallet}
+                seedPhrase={seedPhrase}
+                setSeedPhrase={setSeedPhrase}
+                selectedChain={selectedChain}
+              />
+            }
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/recover"
+            element={
+              <Recover setSeedPhrase={setSeedPhrase} setWallet={setWallet} />
+            }
+          />
+          <Route
+            path="/yourwallet"
+            element={
+              <Create setSeedPhrase={setSeedPhrase} setWallet={setWallet} />
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="content">
+        <h2> Hi there.. </h2>
+        <h4 className="h4"> Welcome to your Web3 Wallet</h4>
+        <Button
+          onClick={() => navigate("/yourwallet")}
+          className="frontPageButton"
+          type="primary"
+        >
+          Create A Wallet
+        </Button>
+        <Button
+          onClick={() => navigate("/recover")}
+          className="frontPageButton"
+          type="default"
+        >
+          Sign In With Seed Phrase
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
